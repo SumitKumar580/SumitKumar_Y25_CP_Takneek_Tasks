7ae0af0d44f3d65e20557a64cefdd57d89d517c1
SumitKumar580 <sumitjee513@gmail.com>
2025-08-06 23:46:20 +0530

    Update Q1_RunForFun.cpp

--- SumitKumar_Task_1/Q1_RunForFun.cpp
+++ SumitKumar_Task_1/Q1_RunForFun.cpp
@@ -1,1 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// taking input
+	
+	int x, y;
+	cin>> x >> y;
+	
+	/*we need to know how many x kms are there in y kms, so for that we use floor division,
+	but if y is a multiple of x then we dont need to count last one, so y-1*/
+	
+	int rest = (y-1)/x;
+	
+	cout<< rest;
+
+}
 
